2f3768a311b974b14c452c248736b6a980a8f5d5
Prem Singh <premsingh@Admins-MacBook-Pro.local>
2025-07-31 12:55:00 +0530

    firstcommit

---
+++ src/app/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Data } from '../data/data';
+
+@Component({
+  selector: 'app-footer',
+  imports: [Data],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {
+
+}
